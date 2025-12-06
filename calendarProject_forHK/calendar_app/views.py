4282b47bce59bc15171673f2ba4af3d52fa2bd13
usudon <sslive88@outlook.jp>
2025-12-06 17:37:25 +0900

    home.htmlを開くように設定変更

--- calendarProject_forHK/calendar_app/views.py
+++ calendarProject_forHK/calendar_app/views.py
@@ -4,6 +4,6 @@ from django.views import View
 
 class IndexView(View):
     def get(self,request):
-        return render(request,"calendar_app/index.html")
+        return render(request,"calendar_app/home.html")
 
 index=IndexView.as_view()
